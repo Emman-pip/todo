8b822cc03ddf4795e4444b3b8825bbbe7c9bc247
emman-pip <parrenoemmanuel756@gmail.com>
2023-01-17 08:56:02 +0800

    Add data factory and global-like array

--- src/index.js
+++ src/index.js
@@ -30,12 +30,31 @@ const structure = (() => {
 
   const content = document.createElement("div");
   const text = document.createElement("div");
+
   topScreen.appendChild(text);
   text.classList.add("contentText");
   text.textContent = "Tasks";
+
   const addTask = document.createElement("button");
   addTask.textContent = "+Add";
+
   topScreen.appendChild(addTask);
   screen.appendChild(content);
   content.classList.add("content");
+
+  addTask.onclick = () => {};
+})();
+
+//data factory
+const dataFactory = (title, description, due) => {
+  return { title, description, due };
+};
+
+//array function
+const array = (() => {
+  const emptyArray = [];
+  if (localStorage.getItem("storage") == undefined) {
+    localStorage.setItem("storage", JSON.stringify(emptyArray));
+  }
+  const array = JSON.parse(localStorage.getItem("storage"));
 })();
