b21c6d7010b220502b3433a64b160a9ed24e02f5
emman-pip <parrenoemmanuel756@gmail.com>
2023-01-17 08:53:48 +0800

    Finish initial structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import _ from "lodash";
 import "./style.css";
 
-const sample = document.createElement("p");
-sample.textContent = "Hello";
-sample.classList.add("sample");
-document.body.appendChild(sample);
+const structure = (() => {
+  const topBar = document.createElement("div");
+  document.body.appendChild(topBar);
+  topBar.textContent = "TodoList.io";
+  topBar.classList.add("topBar");
+
+  const main = document.createElement("div");
+  document.body.appendChild(main);
+  main.classList.add("main");
+
+  const sidebar = document.createElement("div");
+  sidebar.classList.add("sidebar");
+  main.appendChild(sidebar);
+
+  const addProject = document.createElement("div");
+  addProject.textContent = "add project";
+  addProject.classList.add("addProject");
+  sidebar.appendChild(addProject);
+
+  const screen = document.createElement("div");
+  main.appendChild(screen);
+  screen.classList.add("screen");
+
+  const topScreen = document.createElement("div");
+  screen.appendChild(topScreen);
+  topScreen.classList.add("topScreen");
+
+  const content = document.createElement("div");
+  const text = document.createElement("div");
+  topScreen.appendChild(text);
+  text.classList.add("contentText");
+  text.textContent = "Tasks";
+  const addTask = document.createElement("button");
+  addTask.textContent = "+Add";
+  topScreen.appendChild(addTask);
+  screen.appendChild(content);
+  content.classList.add("content");
+})();
